7f20582c0de490495c450a5250df463e10dd5d94
jaquelineflores <floresramos.ej@gmail.com>
2019-05-26 15:26:22 -0700

    pff

--- notariaBC/views/directorio.aspx.cs
+++ notariaBC/views/directorio.aspx.cs
@@ -5,13 +5,13 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace notariaBC
+namespace notariaBC.views
 {
     public partial class directorio : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
--- notariaBC/views/directorio.aspx.designer.cs
+++ notariaBC/views/directorio.aspx.designer.cs
@@ -7,18 +7,9 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
-namespace notariaBC {
+namespace notariaBC.views {
     
     
     public partial class directorio {
-        
-        /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
